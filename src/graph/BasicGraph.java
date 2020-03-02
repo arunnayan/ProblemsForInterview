c7629dbca176e04d62fbd7c12ec9746e651694f4
Arun Singh <arunnayan.28@gmail.com>
2020-03-01 19:37:45 -0800

    Graph

---
+++ src/graph/BasicGraph.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class BasicGraph {
+
+  private LinkedList<Integer> vertex_list[];
+  private int num_of_vertices;
+
+  BasicGraph(int v) {
+    num_of_vertices=v;
+    vertex_list=new LinkedList[v];
+    for(int i=0; i<v;i++){
+      vertex_list[i] = new LinkedList<>();
+    }
+
+    public void add_edges(int u)
+
+
+
+  }
+  public static void main(String[] args) {
+
+  }
+}
